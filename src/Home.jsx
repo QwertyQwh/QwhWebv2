aca9611b74167e3af4f2c8c49db457cf9042c070
QwertyQwh <qinweihang19988@Outlook.com>
2023-08-11 13:27:41 -0400

    v2.1

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,17 @@
 import { useRef,useState,useEffect,Suspense,useContext } from 'react'
+import Svg_ShapeBg from './assets/svg/shape_bg.svg'
+import Svg_ShapeLaptop from './assets/svg/shape_laptop.svg'
 export default function Home(){
 
 
 
-  return }
+return (<>
+  <div className='homeBg'>
+   <span className='homeShapes'>
+  <Svg_ShapeLaptop />
+  </span>
+  <span className='homeHoles'>
+  <Svg_ShapeBg />
+  </span>
+  </div>
+</>)}
